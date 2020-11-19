acdedd1df1751f62599eab31292f03a084c5976a
MeenalAnand <meenalanand@git.com>
2020-11-19 17:38:03 +0530

    My first commit of java project

---
+++ MyJavaProject/src/com/meenal/demo/ClientApp.java
@@ -0,0 +1,13 @@
+package com.meenal.demo;
+
+public class ClientApp {
+	
+	public static void main(String[] args) {
+		
+		Person p= new Person("Jay",101,90.00);
+		System.out.println(p);
+		
+		
+	}
+
+}
